1d88b896e8f221f88a63f47d15616b2c5b7637fe
Kate Laney <kate.a.laney@gmail.com>
2019-11-08 15:19:19 -0500

    updating index, css

--- burgers_db.sql
+++ burgers_db.sql
@@ -13,6 +13,3 @@ CREATE TABLE burgers (
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Double Bacon Cheeseburger", FALSE), ("Spicy Chicken Sandwich", FALSE), ("Jalapeno Burger", TRUE);
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'Bu7g37';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Bu7g37';
